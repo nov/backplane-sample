9da4e26e0fc8e0cdba5a8e65db3a3ac01ccb5233
nov <nov.matake@gree.net>
2013-05-14 17:49:18 +0900

    backplane token and engage token is different

--- app/controllers/engage/tokens_controller.rb
+++ app/controllers/engage/tokens_controller.rb
@@ -1,16 +1,22 @@
 class Engage::TokensController < ApplicationController
   def create
-    token = Rack::OAuth2::AccessToken::Bearer.new(access_token: params[:token])
-    message = {
-      source: root_url,
-      type: 'identity/login',
-      payload: {
-        context: 'hello'
+    engage_token = Rack::OAuth2::AccessToken::Bearer.new(access_token: params[:token])
+    backplane_token, backplane_channel, backplane_refresh_token = cookies[:'backplane2-channel'].split(':')
+    backplane_token = Rack::OAuth2::AccessToken::Bearer.new(access_token: backplane_token)
+    backplane_message = {
+      message: {
+        type: 'identity/login',
+        sticky: false,
+        bus: 'gree.net',
+        channel: backplane_channel,
+        payload: {
+          context: 'hello'
+        }
       }
     }
-    token.post(
-      params[:channel],
-      message.to_json,
+    backplane_token.post(
+      'https://backplane1.janrainbackplane.com/v2/message',
+      backplane_message.to_json,
       'Content-Type' => 'application/json'
     )
     render text: 'hello'
